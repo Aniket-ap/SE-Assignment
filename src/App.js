9f83842571232e9c2f032a731fc5f836e04555a2
Aniket Pradhan <aniket_pro@outlook.com>
2020-12-03 11:54:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import "../App.css";
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Provider } from "../context/Context";
-import Header from "../components/Header";
-import Home from "../components/Home";
-import Trending from "../components/Trending";
-import TopRated from "../components/TopRated";
-import Popular from "../components/Popular";
-import UpComing from "../components/UpComing";
-import Details from "../components/Details";
-import Error from "../service/Error";
-import TopRatedSeries from "../components/TopRatedSeries";
-import PopularSeries from "../components/PopularSeries";
-import AiringToday from "../components/AiringToday";
-import AboutToAir from "../components/AboutToAir";
-import SeriesDetails from "../components/SeriesDetails";
-import Footer from "../components/Footer";
+import { Provider } from "./context/Context";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Trending from "./components/Trending";
+import TopRated from "./components/TopRated";
+import Popular from "./components/Popular";
+import UpComing from "./components/UpComing";
+import Details from "./components/Details";
+import Error from "./service/Error";
+import TopRatedSeries from "./components/TopRatedSeries";
+import PopularSeries from "./components/PopularSeries";
+import AiringToday from "./components/AiringToday";
+import AboutToAir from "./components/AboutToAir";
+import SeriesDetails from "./components/SeriesDetails";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
